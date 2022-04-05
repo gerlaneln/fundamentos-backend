6cc5dccb329cdaac67f0018fec791ecc7cd28b55
Gerlâne Lima <gerlima96@gmail.com>
2022-04-04 20:26:20 -0500

    Primeiro Servlet

--- sgcm/src/main/java/br/ufac/sgcm/PrimeiroServlet.java
+++ sgcm/src/main/java/br/ufac/sgcm/PrimeiroServlet.java
@@ -1,7 +1,28 @@
 package br.ufac.sgcm;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServlet;
 
 public class PrimeiroServlet extends HttpServlet{
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        
+        PrintWriter saida = res.getWriter();
+        saida.println("<html>");
+        saida.println("<head>");
+        saida.println("<title>Meu Primeiro Servlet</title>");
+        saida.println("</head>");
+        saida.println("<body>");
+        saida.println("<h1>Exemplo de Servlet</h1>");
+        saida.println("</body>");
+        saida.println("</html>");
+
+    }
     
 }

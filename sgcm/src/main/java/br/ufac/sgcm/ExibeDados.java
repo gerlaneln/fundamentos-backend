8ce322b7a841cda34086a51b99d4579709948a6b
Gerlâne Lima <gerlima96@gmail.com>
2022-04-05 20:05:48 -0500

    Aula dia 5 de abril

---
+++ sgcm/src/main/java/br/ufac/sgcm/ExibeDados.java
@@ -0,0 +1,27 @@
+package br.ufac.sgcm;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ExibeDados extends HttpServlet{ 
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { //dopost
+        
+        String mensagem = req.getParameter("mensagem");
+
+        PrintWriter saida = resp.getWriter();
+        saida.println("<html>");
+        saida.println("<body>");
+        saida.println("<p>" + mensagem + "</p>");
+        saida.println("</body>");
+        saida.println("</html>");
+
+    }
+
+}
--- sgcm/src/main/webapp/index.jsp
+++ sgcm/src/main/webapp/index.jsp
@@ -1,5 +1,9 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<form action="exibeDados" method=post>
+    <input type="text" name="mensagem"/>
+    <input type="submit" value="Enviar"/>
+</form>
 </body>
 </html>
